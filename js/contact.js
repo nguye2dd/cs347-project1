3c9bc6b77b55e1a504291cb52baebfd57907fa2c
Dylan Nguyen <nguye2dd@dukes.jmu.edu>
2020-10-12 18:38:21 -0400

    added validation to contatc page form, implemented all but services page

--- js/contact.js
+++ js/contact.js
@@ -7,18 +7,41 @@ const details = document.getElementById('details');
 const send = document.getElementById('send');
 
 send.addEventListener('click', () => {
-  let message = 'You have enetered the following information\n\n';
-  message += ('Name: ' + name.value + '\n'); // display name
-  message += ('Phone Number: ' + phone.value + '\n'); // display phone number
-  message += ('E-mail: ' + email.value + '\n'); // display email
-  message += ('Preferred Contact: ' + getSelectedRadioButton(radioButtons) +'\n'); // display preferred method of contact 
-  message += ('Desired Service: ' + service.value + '\n'); //  display desired service
-  //message += ('File uploaded: ' + '\n'); // TODO display if image is uploaded or not??? how to do that??
-  message += ('Additional Details: \n' + details.value + '\n\n'); // display additional details
-  message += ('Thank you for choosing Sophisticuts for your beauty needs. We will be in touch ASAP!');
 
-  alert(message);
+  if (!missingRequiredField(name.value, phone.value, email.value, getSelectedRadioButton(radioButtons)))  {
+    let message = 'You have enetered the following information\n\n';
+    message += ('Name: ' + name.value + '\n'); // display name
+    message += ('Phone Number: ' + phone.value + '\n'); // display phone number
+    message += ('E-mail: ' + email.value + '\n'); // display email
+    message += ('Preferred Contact: ' + getSelectedRadioButton(radioButtons) +'\n'); // display preferred method of contact 
+    message += ('Desired Service: ' + service.value + '\n'); //  display desired service
+    //message += ('File uploaded: ' + '\n'); // TODO display if image is uploaded or not??? how to do that??
+    message += ('Additional Details: \n' + details.value + '\n\n'); // display additional details
+    message += ('Thank you for choosing Sophisticuts for your beauty needs. We will be in touch ASAP!');
+
+    alert(message);
+  }
+
 });
+
+function missingRequiredField(name, phone, email, method) {
+  let missing = false;
+
+  if (name.trim() === '') {
+    missing = true;
+    confirm("Please provide a name.");
+  } 
+  if (phone.trim() === '' && email.trim() === '') {
+    missing = true;
+    confirm('Please provide a phone number or email address.');
+  }
+  if (method.trim() === '') {
+    missing = true;
+    confirm('Please select a preferred method of contact.');
+  }
+
+  return missing;
+}
 
 function getSelectedRadioButton(radioButtons) {
   let retVal = '';
